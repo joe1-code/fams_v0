faef6fdf4186cde7df7ae430a84ca954cbc20c4d
Joel John <john4m2@gmail.com>
2025-05-01 00:30:48 +0300

    first commit

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * class HomeController
+     * 
+     * This controller is implemented to handle all the methods responsible with auth0 
+     * 2025-04-30
+     * 
+     * @author Joel John Njaghah
+     * @package App\Http\Controllers
+     * @version v1.0.0
+     * @see https://auth0.com/docs/quickstart/webapp/laravel
+     */
+}
